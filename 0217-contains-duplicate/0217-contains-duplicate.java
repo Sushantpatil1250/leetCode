4d73fce3de1a6645230d62428775289827ef03d0
Sushantpatil1250 <patilsushant348@gmail.com>
2026-09-04 14:36:01 +0530

    Time: 18 ms (77.48%), Space: 108.3 MB (29.32%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -3,14 +3,24 @@
 class Solution {
     public boolean containsDuplicate(int[] arr) {
 
-        Arrays.sort(arr);
+        // Arrays.sort(arr);
 
-        for(int i = 0; i < arr.length - 1; i++) {
-            if(arr[i] == arr[i + 1]) {
+        // for(int i = 0; i < arr.length - 1; i++) {
+        //     if(arr[i] == arr[i + 1]) {
+        //         return true;
+        //     }
+        // }
+
+        // return false;
+
+        HashSet <Integer> a=new HashSet<>();
+        for (int i=0;i<arr.length;i++){
+            if(a.contains(arr[i])){
                 return true;
+            }else{
+                a.add(arr[i]);
             }
         }
-
         return false;
     }
 }
