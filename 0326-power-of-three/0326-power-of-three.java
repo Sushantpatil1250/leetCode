53da5bbf05fc0b370ded2d7cd322d4eb665eaf2c
Sushantpatil1250 <patilsushant348@gmail.com>
2026-09-21 11:57:55 +0530

    Time: 8 ms (92.4%), Space: 45.9 MB (83.19%) - LeetHub

--- 0326-power-of-three/0326-power-of-three.java
+++ 0326-power-of-three/0326-power-of-three.java
@@ -3,15 +3,22 @@ class Solution {
         if (n <= 0) {
             return false;
         }
-        else{
-            for(int i = 0 ; n>=Math.pow(3 ,i) ; i++){
-                double x =Math.pow(3 ,i);
-                if(x == n){
-                    return true;
-                }
-            }
+        // else{
+        //     for(int i = 0 ; n>=Math.pow(3 ,i) ; i++){
+        //         double x =Math.pow(3 ,i);
+        //         if(x == n){
+        //             return true;
+        //         }
+        //     }
+        // }
+        // return false;
+
+        while(n % 3 ==0){
+            n = n/3;
         }
-        return false;
+
+        if( n == 1) return true;
+        else return false;
 
     }
 }
